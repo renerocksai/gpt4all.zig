410624d4bac879f5ce7373d14200769c884ac948
Rene Schallner <rene@renerocks.ai>
2023-04-10 00:54:29 +0200

    updated README, thx mattnite in build.zig

--- build.zig
+++ build.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 
+// thanks to github.com/mattnite !!!
 const libcurl = @import("zig-libcurl/libcurl.zig");
 const mbedtls = @import("zig-libcurl/zig-mbedtls/mbedtls.zig");
 const zlib = @import("zig-libcurl/zig-zlib/zlib.zig");
@@ -9,6 +10,7 @@ pub fn build(b: *std.build.Builder) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    // thanks to github.com/mattnite !!!
     const z = zlib.create(b, target, optimize);
     const tls = mbedtls.create(b, target, optimize);
     const ssh2 = libssh2.create(b, target, optimize);
@@ -18,20 +20,21 @@ pub fn build(b: *std.build.Builder) !void {
     ssh2.link(curl.step);
     tls.link(curl.step);
     z.link(curl.step, .{});
-    // curl.step.install();
 
     const exe = b.addExecutable(.{
         .name = "chat",
+        // we added zig code to download models
         .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
         .optimize = optimize,
     });
+
+    // now, here comes the C and C++ stuff for the actual chat client
     exe.addIncludePath(".");
     exe.addCSourceFile("ggml.c", &.{ "-std=c11", "-D_POSIX_C_SOURCE=199309L", "-pthread" });
     exe.addCSourceFiles(&.{ "utils.cpp", "chat.cpp" }, &.{"-std=c++11"});
     curl.link(exe, .{});
     exe.linkLibC();
     exe.linkLibCpp();
-
     exe.install();
 }
